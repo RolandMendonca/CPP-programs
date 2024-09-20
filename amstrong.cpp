11e212a9f819f39d61c500fede9f08aed3eb029b
Roland <mendoncaroland1999@gmail.com>
2024-09-20 12:59:22 +0530

    commit

--- amstrong.cpp
+++ amstrong.cpp
@@ -4,10 +4,24 @@ using namespace std;
 
 int main()
 {
-    int num;
+    int num,temp,sum=0,n;
 
     cout << "Enter a number:-";
     cin >>num;
+
+    while (n>0)
+    {
+        temp = n%10;
+        sum = sum + (temp*temp*temp);
+        n = n/10;
+    }
+    
+    if (sum == num)
+    {
+        cout <<"Amstrong number";
+    }
+    else 
+        cout <<"Not an amstrong number";
 
     
 
